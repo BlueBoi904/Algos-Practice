1b5ce84a7517e62a270e35f72bd90d126086cf37
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-28 10:32:53 -0400

    Implmented queue using linked list

---
+++ Stacks-Queues/queue.js
@@ -0,0 +1,57 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.first;
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  dequeue() {
+    if (!this.first) {
+      return null;
+    } else if (this.first === this.last) {
+      this.last = null;
+    }
+    const holdingPointer = this.first;
+    this.first = this.first.next;
+    this.length--;
+    return holdingPointer;
+  }
+  isEmpty() {
+    if (this.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+const myQueue = new Queue();
+myQueue.enqueue("Joy");
+myQueue.enqueue("Wendy");
+myQueue.enqueue("Yeri");
+myQueue.enqueue("Seulgi");
+console.log(myQueue.peek());
+myQueue.dequeue();
+console.log(myQueue.peek());
