3bc0ea27d401d57ac9ea59bebe2fb95687d8bb54
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-28 12:20:01 -0400

    Added rules and notes about binary trees

---
+++ Trees/binaryTree.js
@@ -0,0 +1,25 @@
+/*
+Rules:
+
+1) Each node can only have either 0, 1, or 2 nodes
+2) Each child can only have one parent
+
+Greate for searching or comparing things
+
+O(log n) runtime, better than O(n)
+
+Balanced (Basicly turns into a linked list) - O(log n)
+Unbalaned - O(n)
+
+Pros:
+Better than O(n)
+
+Ordered
+
+Flexible Size
+
+Cons:
+
+No O(1) operations
+
+*/
