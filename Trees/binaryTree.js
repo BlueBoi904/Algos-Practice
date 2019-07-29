963ffdb963fb57ba66916d828fc2e6cadac8718d
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-28 21:45:18 -0400

    Implemented binary search tree insert method

--- Trees/binaryTree.js
+++ Trees/binaryTree.js
@@ -23,3 +23,71 @@ Cons:
 No O(1) operations
 
 */
+
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      // Create non stop while loop
+      while (true) {
+        //Check for left case
+        if (value < currentNode.value) {
+          //Left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        }
+        //Check for right case
+        else {
+          // Right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  lookup(value) {
+    //Code here
+  }
+  // remove
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+JSON.stringify(traverse(tree.root));
+
+//     9
+//  4     20
+//1  6  15  170
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
