f633705dac5c0c1e797455886001f638c78cc3bb
Benji Mirschel <cliff.mirschel@gmail.com>
2019-08-05 08:12:54 -0400

    Fib solved

---
+++ Recursion/main.js
@@ -0,0 +1,11 @@
+/*
+
+3 Rules of Recursion:
+
+1. Identify the base case
+
+2. Identify the recursive case
+
+3.Get closer and closer and return when needed.
+Usually you have 2 returns, 1 for base case, 1 for function
+*/
